24f5996180e15c01a831bbf50610ba45d9246545
kunal meshram <kunalkmeshram19@gmail.com>
2025-01-04 18:10:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,8 +13,10 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "*" ,
       "http://localhost:8000",
       "http://localhost:5173",
+      
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
